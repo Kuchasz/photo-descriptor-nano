8b25249bc54a1398e11dedf389df0f33876a1955
Rafal Pysz <kuchasz1@o2.pl>
2017-01-15 13:44:40 +0100

    alias option added

--- src/Components/ImagesExporter/Index.ts
+++ src/Components/ImagesExporter/Index.ts
@@ -3,11 +3,16 @@ import Component from 'vue-class-component';
 @Component({
     template: `
       <md-layout md-flex class="images-exporter">
+        <md-input-container>
+            <label>Alias do galerii</label>
+            <md-input v-model="galleryAlias" placeholder="Wpisz alias do galerii"></md-input>
+        </md-input-container>
         <md-button @click="exportSql" class="md-raised md-primary">Export do SQL</md-button>
       </md-layout>
 `
 })
 export class ImagesExporter extends Vue {
+    galleryAlias: string;
     exportSql() {
         this.$emit('export-images');
     }
